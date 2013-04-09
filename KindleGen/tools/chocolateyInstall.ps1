373d68e9738c9d607b55d968614007b364ea2568
Alexey Matashkin <amatashkin@gmail.com>
2013-04-09 10:25:17 +0400

    adding KindleGen

---
+++ KindleGen/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
+
+$packageName = 'KindleGen' # arbitrary name for the package, used in messages
+$url = 'http://s3.amazonaws.com/kindlegen/kindlegen_win32_v2_8.zip' # download url
+
+# download and unpack a zip file
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
